7839f79b2322ca563310407a7453db8f71e397df
William C. Santos <william.santos@wssim.com.br>
2016-08-11 07:44:59 -0300

    Criação de exemplo utilizando Padrão Singleton

--- Duelo/src/br/univel/duelo/DueloBuilder.java
+++ Duelo/src/br/univel/duelo/DueloBuilder.java
@@ -3,6 +3,7 @@ package br.univel.duelo;
 import br.univel.duelo.arma.Arma;
 import br.univel.duelo.arma.ArmaFactory;
 import br.univel.duelo.arma.TipoArma;
+import br.univel.duelo.lojamunicao.LojaMunicao;
 import br.univel.duelo.pistoleiro.Pistoleiro;
 import br.univel.duelo.pistoleiro.PistoleiroFactory;
 import br.univel.duelo.pistoleiro.TipoPistoleiro;
@@ -15,6 +16,7 @@ public class DueloBuilder {
 	private TipoPistoleiro tipo2;
 	private PistoleiroFactory pistoleiroFactory;
 	private ArmaFactory armaFactory;
+	private LojaMunicao lojaMunicao;
 
 	public String getNome1() {
 		return this.nome1;
@@ -70,11 +72,22 @@ public class DueloBuilder {
 		return this;
 	}
 
+	public LojaMunicao getLojaMunicao() {
+		return lojaMunicao;
+	}
+
+	public DueloBuilder setLojaMunicao(LojaMunicao lojaMunicao) {
+		this.lojaMunicao = lojaMunicao;
+		return this;
+	}
+
 	public Duelo build() {
 		final Pistoleiro pistoleiro1 = this.pistoleiroFactory.create(tipo1, nome1);
 		final Pistoleiro pistoleiro2 = this.pistoleiroFactory.create(tipo2, nome2);
 		final Arma arma1 = this.armaFactory.create(TipoArma.LONGA);
 		final Arma arma2 = this.armaFactory.create(TipoArma.LONGA);
+		arma1.recarregar(this.lojaMunicao.venda(10));
+		arma2.recarregar(this.lojaMunicao.venda(10));
 		pistoleiro1.setArma(arma1);
 		pistoleiro2.setArma(arma2);
 		return new Duelo(pistoleiro1, pistoleiro2);
